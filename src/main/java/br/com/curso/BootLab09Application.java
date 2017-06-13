e99c87df328754ce5fddf904c86b8426dc234f8c
Paulo Pacheco <paulorb.pacheco@gmail.com>
2017-06-12 21:27:32 -0300

    first commit

---
+++ src/main/java/br/com/curso/BootLab09Application.java
@@ -0,0 +1,12 @@
+package br.com.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLab09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootLab09Application.class, args);
+	}
+}
